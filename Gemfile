7fdcb0c6747e94288e1a4868768437f12c92d8be
Luciano Hoffmaister Ribeiro <hr.luciano@gmail.com>
2012-06-22 00:29:00 -0300

    reconfigurado Gemfile

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '3.2.1'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'mysql2'
-
+gem 'bigdecimal'
 
 # Gems used only for assets and not required
 # in production environments by default.
@@ -35,4 +35,4 @@ gem 'unicorn'
 gem 'capistrano'
 
 # To use debugger
-gem 'ruby-debug19', :require => 'ruby-debug'
+#gem 'ruby-debug19', :require => 'ruby-debug'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,47 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require "bundler/capistrano"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+server = "", :web, :app, :db, primary: true
+set :application, "demo-tasklist"
+set :user, "ec2-user"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :user_sudo, false
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :scm, :git
+set :repository,  "git@github.com:lucianohr/#{application}.git"
+set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy_cleanup"
+
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "service unicorn_#{application} #{command}"
+    end
+  end
+  
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/"
+    sudo "ln -nfs #{current_path}/config/unicorn.conf /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Editando arquivos de configuracao #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+  
+  task :symlink_config, roles: :app do
+    run ""
+  end
+end
+
+# role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+# role :app, "your app-server here"                          # This may be the same as your `Web` server
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
